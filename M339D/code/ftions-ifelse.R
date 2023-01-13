00dceceebaa39aa717a5674110801ac9b5d23aa4
Milica Cudina <mcudina@math.utexas.edu>
2023-01-13 16:09:00 -0600

    No_message

--- M339D/code/ftions-ifelse.R
+++ M339D/code/ftions-ifelse.R
@@ -1,17 +1,17 @@
-mult<-function(x,y){
+mult<-function(x, y){
   x*y
 }
 
 mult(2,3)
 
-replicate(3,mult(1,4))
+replicate(3, mult(1,4))
 
-maximum<-function(x,y){
-  if(x>y){
+maximum<-function(x, y){
+  if (x>y){
     print(x)
   } else {
     print(y)
   }
 }
 
-maximum(exp(1), log(10))
+maximum(exp(2), sqrt(pi))
