d1dd09e5648c27a46afeecd2e0dc7cb9fec66bf2
Milica Cudina <mcudina@math.utexas.edu>
2023-08-29 14:34:32 -0500

    No_message

--- M358K/code/war-etc.R
+++ M358K/code/war-etc.R
@@ -21,6 +21,11 @@ many.wars=replicate(nsim, war())
 many.wars=="tie"
 sum(many.wars=="tie")/nsim
 
+wars.tab=table(many.wars)
+
+barplot(wars.tab, main="Many games of war", 
+        col="steelblue")
+
 #####################################################
 
 dbinom(0,size=1,prob=0.5)
@@ -41,4 +46,6 @@ mean(sims==1)
 mean(sims==2)
 
 new.sims=rbinom(nsim, size=20, prob=0.4)
-hist(new.sims, breaks=seq(-0.5,20.5,1))
+hist(new.sims, breaks=seq(-0.5,20.5,1), 
+     main="Binomial histogram", 
+     col="Bisque")
