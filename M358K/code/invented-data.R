331d9b034b8305ff37d2d6c62524d364f1276aca
Milica Cudina <mcudina@math.utexas.edu>
2023-09-08 14:34:52 -0500

    No_message

--- M358K/code/invented-data.R
+++ M358K/code/invented-data.R
@@ -1,26 +1,36 @@
+#synthetic data
 data<-c(sample(1:100, 50, replace=TRUE), rbinom(100, size=25, prob=0.4))
+data
 
+#histogram
 hist(data)
 hist(data, breaks=25)
-hist(data, breaks=5)
 hist(data, breaks=50)
+hist(data, breaks=5)
 
-bp=boxplot(data, col="salmon", pch=19, horizontal=TRUE)
-bp$out
-
+#stacked dot plot
 stripchart(data, method="stack",
            at=0,
-           pch=20,
+           pch=19,
            col="steelblue",
-           main="Stacked dotplot",
-           xlab="Toy data",
+           main="Stacked Dot Plot",
+           xlab="Toy Data",
            ylab="Counts")
 
+#boxplot
+bp=boxplot(data, 
+           col="peru", 
+           pch=19#,
+           #horizontal=TRUE
+           )
+bp$out
+
+#numerical summary
 summary(data)
 
 var(data)
-s.squared=sum((data-mean(data))^2)/(length(data)-1)
-s.squared
-
 sd(data)
-sqrt(s.squared)
+
+s.squared=sum((data-mean(data))^2)/(length(data)-1)
+s=sqrt(s.squared)
+s
