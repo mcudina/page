40ed09337f95be5a31638600737faa902355fe59
Milica Cudina <mcudina@math.utexas.edu>
2023-11-13 16:21:42 -0600

    No_message

--- M358K/code/dobson-ozone.R
+++ M358K/code/dobson-ozone.R
@@ -1,9 +1,10 @@
 dobson=c(269,246, 388, 354, 266, 303, 259, 295, 274, 249,
          271, 254)
+length(dobson)
+
 hist(dobson)
 boxplot(dobson)
 qqnorm(dobson,col="blue",xlim=c(-3,3),ylim=c(100,450)) 
 qqline(dobson,col="red",lwd=2)
 
-#small p-value => likely not normal
 shapiro.test(dobson)
