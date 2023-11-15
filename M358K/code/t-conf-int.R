38e25043139f21ef3fd31ca4fd6208088f426c2c
Milica Cudina <mcudina@math.utexas.edu>
2023-11-15 14:47:57 -0600

    No_message

--- M358K/code/t-conf-int.R
+++ M358K/code/t-conf-int.R
@@ -1,6 +1,7 @@
 # data set 
 data<-c(17.2, 15.7, 24.9, 16.2, 18.5, 22.8)
 n=length(data)
+n
 
 #assume that you know these come from a normal distribution
 #we want an 80% confidence interval
@@ -8,15 +9,17 @@ C=0.80
 
 ## method I
 x.bar=mean(data)
+x.bar
 s=sd(data)
+s
 t.star=qt((1+C)/2, df=n-1)
 t.star
 
-##lower bound of CI
+###lower bound of CI
 lower.bd=x.bar-t.star*s/sqrt(length(data))
 lower.bd
 
-##upper bound of CI
+###upper bound of CI
 upper.bd=x.bar+t.star*s/sqrt(length(data))
 upper.bd
 
