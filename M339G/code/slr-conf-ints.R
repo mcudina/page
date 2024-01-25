6d355d85278cb6fe8f4ecaa3645d73c8b7e6ecd4
Milica Cudina <mcudina@math.utexas.edu>
2024-01-24 18:10:09 -0600

    No_message

--- M339G/code/slr-conf-ints.R
+++ M339G/code/slr-conf-ints.R
@@ -1,4 +1,4 @@
-beta.0<-2.5  ### theoretical paramater values
+beta.0<-2.5  ### theoretical parameter values
 beta.1<-0.5
 curve(beta.0+beta.1*x, 0, 25, lwd=3, col="green")
 
@@ -20,12 +20,16 @@ x.coords
 y.coords<-beta.0 + beta.1*x.coords+errors
 y.coords
 
-plot(x.coords,y.coords, cex=2, pch=20, col="blue", main="Simple least squares", ylab="Response", xlab="Explanatory", xlim=c(0, domain), ylim=c(0,beta.1*domain+beta.0))
+plot(x.coords,y.coords, cex=2, pch=20, col="blue", 
+     main="Simple least squares", 
+     ylab="Response", xlab="Explanatory", 
+     xlim=c(0, domain), ylim=c(0,beta.1*domain+beta.0))
 
 abline(lm(y.coords ~ x.coords), lwd=1.5, col="red")
 abline(beta.0,beta.1, lwd=2, col="green")
 
 reg<-lm(y.coords ~ x.coords)
+reg
 
 reg.all<-summary(reg)
 reg.all
