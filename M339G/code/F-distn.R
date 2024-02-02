ebe3d170381118f6232b857730dc3cbf1fbf4103
Milica Cudina <mcudina@math.utexas.edu>
2024-02-02 16:38:34 -0600

    No_message

--- M339G/code/F-distn.R
+++ M339G/code/F-distn.R
@@ -1,8 +1,10 @@
-#simulating some values from the F distribution
-n.sims=10^6
-data=rf(n.sims, 3,2)
-hist(data, breaks=25)
+#plotting the density 
+curve(df(x, 2, 3), from=0, to= 10)
+curve(df(x, 3, 2), from=0, to= 10)
+
+#simulating from F
+n.sim=100000
+data=rf(n.sim, 3, 2)
+hist(data)
 summary(data)
 boxplot(data)
-#the density
-curve(df(x, 3, 2), from=0, to=100)
