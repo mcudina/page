4c589b85891bf9c6a0d3abd508d5fffd917c7d79
Milica Cudina <mcudina@math.utexas.edu>
2024-10-11 14:42:11 -0500

    No message

--- M378K/code/gamma-clt.R
+++ M378K/code/gamma-clt.R
@@ -39,16 +39,16 @@ curve(dgamma(x, k.hat, scale=tau.hat), add=TRUE, col="blue", lwd=2)
 #demo of the Central Limit Theorem
 #define a function which calculates the average of 
 #a simulated batch of gammas
-avg.gamma<-function(n, a, b){
-  sims=rgamma(n, a, b)
+avg.gamma<-function(n, k, tau){
+  sims=rgamma(n, k, scale=tau)
   return(mean(sims))
 }
 #repeat this function many times
 n.batches=7000
-avgs=replicate(n.batches, avg.gamma(nsims, k, scale=tau))
+avgs=replicate(n.batches, avg.gamma(nsims, k, tau))
 #plot the histogram
 hist(avgs, col="bisque", prob=TRUE)
 #theoretical parameters of the bell curve
 mu.norm=k*tau
-sigma.norm=sqrt(k*tau^2)
+sigma.norm=sqrt(k*tau^2)/sqrt(nsims)
 curve(dnorm(x, mean=mu.norm, sd=sigma.norm), lwd=2, col="red", add=TRUE)
