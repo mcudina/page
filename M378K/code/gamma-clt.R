64e6b84965d88cd1c2fb54dc867cf44b7d43a946
Milica Cudina <mcudina@math.utexas.edu>
2024-10-11 14:10:47 -0500

    No message

--- M378K/code/gamma-clt.R
+++ M378K/code/gamma-clt.R
@@ -1,23 +1,25 @@
 #the gamma dist'n
 ?dgamma
 #shape parameter
-alpha=2
-#rate parameter
-beta=1
+k=2
+#scale parameter
+tau=1
 
-nsims=1000
+nsims=10000
 #simulating from the gamma
-sims=rgamma(nsims, alpha, beta)
+sims=rgamma(nsims, k, scale=tau)
 mu.hat=mean(sims)
+mu.hat
 v.hat=var(sims)
-beta.hat=mu.hat/v.hat
-beta.hat
-alpha.hat=mu.hat*beta.hat
-alpha.hat
+v.hat
+tau.hat=v.hat/mu.hat
+tau.hat
+k.hat=mu.hat/tau.hat
+k.hat
 #plotting the histogram
-hist(sims, breaks=25, prob=TRUE, ylim=c(0, dgamma(1, alpha.hat, beta.hat)))
+hist(sims, breaks=25, prob=TRUE, ylim=c(0, dgamma(1, k.hat, scale=tau.hat)))
 #superimposing the theoretical density
-curve(dgamma(x, alpha, beta), add=TRUE, col="purple", lwd=2)
+curve(dgamma(x, k, scale=tau), add=TRUE, col="purple", lwd=2)
 
 #fitting the density curve
 #moment matching
@@ -32,7 +34,7 @@ alpha.hat=mu.hat*beta.hat
 alpha.hat
 
 #plot the fitted density
-curve(dgamma(x, alpha.hat, beta.hat), add=TRUE, col="blue", lwd=2)
+curve(dgamma(x, k.hat, scale=tau.hat), add=TRUE, col="blue", lwd=2)
 
 #demo of the Central Limit Theorem
 #define a function which calculates the average of 
@@ -43,10 +45,10 @@ avg.gamma<-function(n, a, b){
 }
 #repeat this function many times
 n.batches=7000
-avgs=replicate(n.batches, avg.gamma(nsims, alpha, beta))
+avgs=replicate(n.batches, avg.gamma(nsims, k, scale=tau))
 #plot the histogram
 hist(avgs, col="bisque", prob=TRUE)
 #theoretical parameters of the bell curve
-mu.norm=alpha/beta
-sigma.norm=sqrt(alpha)/beta/sqrt(nsims)
+mu.norm=k*tau
+sigma.norm=sqrt(k*tau^2)
 curve(dnorm(x, mean=mu.norm, sd=sigma.norm), lwd=2, col="red", add=TRUE)
